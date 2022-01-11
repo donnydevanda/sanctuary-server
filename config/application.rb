b55a1ba7e21c47dd1db5d2ae8a7970bfb13ae1a8
Muhammad Donny Devanda <devandadonny@gmail.com>
2022-01-11 10:20:20 +0700

    Revert "CORS Update"
    This reverts commit 08d814ba5257090b502b6021a645832360dec1d7.

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module SanctuaryServer
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers: :any, :methods: [:get, :post, :put, :patch, :delete, :options, :head]
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
   end

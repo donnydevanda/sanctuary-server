e5b0772147ad7e7961fb4530271d95dd527fadbe
Muhammad Donny Devanda <devandadonny@gmail.com>
2022-01-11 10:22:46 +0700

    CORS Update

--- config/application.rb
+++ config/application.rb
@@ -19,10 +19,16 @@ module SanctuaryServer
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.middleware.insert_before 0, Rack::Cors do
+    config.api_only = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource(
+          '*',
+          headers: :any,
+          methods: [:get, :patch, :put, :delete, :post, :options]
+          )
       end
     end
   end
